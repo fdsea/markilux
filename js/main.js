7a4e421a766f54ae77dd15d9f2ee2e824cf7f8d5
Vladimir Kukov <uncle-4040@yandex.ru>
2017-04-05 22:37:01 +0400

    slider

---
+++ js/main.js
@@ -0,0 +1,87 @@
+var col_0 =[
+    "/images/awards/award-1.jpg",
+    "/images/awards/award-1.jpg",
+    "/images/awards/award-1.jpg"
+];
+var col_1 =[
+    "/images/awards/award-2.jpg",
+    "/images/awards/award-2.jpg",
+    "/images/awards/award-2.jpg"
+];
+var col_2 =[
+    "/images/awards/award-3.jpg",
+    "/images/awards/award-3.jpg",
+    "/images/awards/award-3.jpg"
+];
+var col_3 =[
+    "/images/awards/award-4.jpg",
+    "/images/awards/award-4.jpg",
+    "/images/awards/award-4.jpg"
+];
+
+function addContent(arr){
+    var owlCar = document.querySelector('.owl-carousel');
+
+for(let i = 0; i < arr.length; i++){
+    var elem = document.createElement('div');
+        elem.innerHTML='<img src='+arr[i]+' alt="">';
+        owlCar.appendChild(elem);
+  }
+}
+
+function removeContent(arr){
+     var owlCar = document.querySelector('.owl-carousel');
+
+for(let i = 0; i < arr.length; i++){
+    var elem = document.createElement('div');
+        elem.innerHTML='<img src='+arr[i]+' alt="">';
+        owlCar.appendChild(elem);
+  }
+}
+
+toggleContent(col_3);
+
+function toggleAllBlock(){
+    var li = document.querySelectorAll('.li');
+    function returnIndex(){
+        var index = Array.prototype.indexOf.call(li, this);
+        return index;
+    }
+   // for(let i = 0;i<li.length;i++){
+      //  li[i].addEventListener('click',()=>{
+        
+    //}
+    
+    li[0].addEventListener('click',()=>{
+        toggleContent(col_0)
+    });
+}
+toggleAllBlock();
+
+
+
+
+
+
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:1,
+            nav:false
+        },
+        1000:{
+            items:1,
+            nav:true,
+            loop:false
+        }
+    }
+})
