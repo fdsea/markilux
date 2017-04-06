7e2edd9cefc0cc2ef08a5b72106a77faee597acb
fdsea <uncle-4040@yandex.ru>
2017-04-06 11:08:38 +0300

    slider

--- js/main.js
+++ js/main.js
@@ -1,87 +1,35 @@
-var col_0 =[
-    "/images/awards/award-1.jpg",
-    "/images/awards/award-1.jpg",
-    "/images/awards/award-1.jpg"
-];
-var col_1 =[
-    "/images/awards/award-2.jpg",
-    "/images/awards/award-2.jpg",
-    "/images/awards/award-2.jpg"
-];
-var col_2 =[
-    "/images/awards/award-3.jpg",
-    "/images/awards/award-3.jpg",
-    "/images/awards/award-3.jpg"
-];
-var col_3 =[
-    "/images/awards/award-4.jpg",
-    "/images/awards/award-4.jpg",
-    "/images/awards/award-4.jpg"
-];
-
-function addContent(arr){
-    var owlCar = document.querySelector('.owl-carousel');
-
-for(let i = 0; i < arr.length; i++){
-    var elem = document.createElement('div');
-        elem.innerHTML='<img src='+arr[i]+' alt="">';
-        owlCar.appendChild(elem);
-  }
-}
-
-function removeContent(arr){
-     var owlCar = document.querySelector('.owl-carousel');
-
-for(let i = 0; i < arr.length; i++){
-    var elem = document.createElement('div');
-        elem.innerHTML='<img src='+arr[i]+' alt="">';
-        owlCar.appendChild(elem);
-  }
-}
-
-toggleContent(col_3);
-
-function toggleAllBlock(){
-    var li = document.querySelectorAll('.li');
-    function returnIndex(){
-        var index = Array.prototype.indexOf.call(li, this);
-        return index;
-    }
-   // for(let i = 0;i<li.length;i++){
-      //  li[i].addEventListener('click',()=>{
-        
-    //}
-    
-    li[0].addEventListener('click',()=>{
-        toggleContent(col_0)
-    });
-}
-toggleAllBlock();
-
-
-
-
-
-
-
-
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:true
-        },
-        600:{
-            items:1,
-            nav:false
-        },
-        1000:{
-            items:1,
-            nav:true,
-            loop:false
-        }
-    }
+	loop: true
+	, margin: 10
+	, responsiveClass: true
+	, responsive: {
+		0: {
+			items: 1
+			, nav: true
+		}
+		, 600: {
+			items: 1
+			, nav: false
+		}
+		, 1000: {
+			items: 1
+			, nav: true
+			, loop: false
+		}
+	}
 })
+
+function toggleTabs() {
+	let li = document.querySelectorAll('.li')
+		, slider_design = document.querySelectorAll('.slider_design');
+	//, slider_content = document.querySelectorAll('slider_content');
+	for (let i = 0; i < li.length; i++) {
+		li[i].addEventListener('click', (e) => {
+			for (let j = 0; j < slider_design.length; j++) {
+				slider_design[j].classList.remove('active_slider_design');
+			}
+			slider_design[i].classList.add('active_slider_design');
+		});
+	}
+}
+toggleTabs();
