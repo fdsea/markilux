dea642d73530704bd52bc526e5366e3df9facabd
fdsea <uncle-4040@yandex.ru>
2017-04-10 15:19:56 +0300

    toggle design menu, move carousel, fix nav menu

---
+++ js/design_nav_menu.js
@@ -0,0 +1,19 @@
+(function(){
+let btn = document.querySelector('.design_button_nav '),
+	li = document.querySelectorAll('.li'),
+	//fa = focument.querySelector('.design_button_nav .fa'),
+	nav_panel = document.querySelector('.left');
+
+btn.addEventListener("click",()=>{
+ nav_panel.classList.toggle('active_mobile_nav_panel');
+ btn.classList.toggle('active_mobile_button_disign');	
+ //fa.classList.toggle('fa-arrow-left');
+	
+ });
+	for(let i = 0; i < li.length;i++){
+li[i].addEventListener('click',()=>{
+	nav_panel.classList.remove('active_mobile_nav_panel');
+	 btn.classList.remove('active_mobile_button_disign');
+   });
+  }	
+}())
