abc157139ee427beaa1844ce26b2ebf5e6813fab
fdsea <uncle-4040@yandex.ru>
2017-04-19 16:29:48 +0300

    куча правок по верстке 1

--- js/slide_page.js
+++ js/slide_page.js
@@ -1,14 +1,18 @@
  $(function(){
 
 var a = document.querySelectorAll('.menu_item_a');
-     for(var i = 0; i < a.length; i++){
+	  
+		 function scrollToSlide(i){
+		 
 			 a[i].addEventListener('click',function(){
 				  var atr = a[i].getAttribute('href').substring(1);
 				 
 				 $('html,body').stop().animate({ scrollTop: $("#"+atr).offset().top }, 1500);
            e.preventDefault();
 			 });
-    
+		 }
+     for(var i = 0; i < a.length; i++){
+		scrollToSlide(i);
   }
 
 });
